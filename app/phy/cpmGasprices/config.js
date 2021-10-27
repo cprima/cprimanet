838240799f4b1acf16ef3360d795823f84fc8fb7
Christian Prior-Mamulyan <cprior@gmail.com>
2021-10-27 07:37:22 +0200

    Google Apps Script cpmGasprices

---
+++ app/phy/cpmGasprices/config.js
@@ -0,0 +1,19 @@
+//put this in e.g. secretes.gs and .gitignore it:
+//var apikey = '00000000-0000-0000-0000-000000000002';
+var ids_csv = '005056ba-7cb6-1ed2-bceb-79dff61a8d26,' + // STAR Nordstadt
+    'c7f10377-ac4b-4a53-8230-ef35a1636d67' + ',' + // Shell Ewersbach
+    'bfa77901-672d-427c-9521-e26064993d9a' + ',' + // Freie Tankstelle Frankenberg
+    '';   // 
+
+//gasprices
+var output_spreadsheetId = "1pkenBWEjfumrBYmsqgk3ms44Bgo8GXaCkdBoEYcp8SY";
+var output_sheetname = 'list';
+var output_visualization_sheetname = 'net.cprima.viz.3fullwks';
+
+var output_chartfilename = "net.cprima.viz.3fullwks.png";
+
+var temp_slidesID = "1uL5RcO6d9poeXzqHDeNT1M9sk5XsnFAYxZaaGID1fSQ";
+
+//put this in e.g. secretes.gs and .gitignore it:
+//var facebook_accesstoken = 'yFAKEmzFwq2Jmu7xqXPeR8vQDaEKw9dCvCNEBhu8C8i3L4JhYSZH75FzVbV8u7HtefNp65PCpdN9X5UmJbF4LNNfrMLeqAgAhKGP8T3Wrq6j75V6jY5Riq5XmZXLcEXzHa2pr3vNdzEiP86B5UzSUS3GJ6XUN5CFKBaaAbMKZTyGVCqrqaSFPqYnRpQ5EEpxFAKE';
+var facebook_baseurl = "https://graph.facebook.com/v10.0/123456789012345/"
